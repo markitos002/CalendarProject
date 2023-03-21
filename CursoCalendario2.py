aeec959f76e77adfe53ba889f576f589539e6711
Marco Fidel Avila <markos.avila@gmail.com>
2023-03-20 23:54:17 -0500

    calendario mes funcionando

--- CursoCalendario2.py
+++ CursoCalendario2.py
@@ -1,8 +1,11 @@
-mes = input("ingrese el mes [1-12]: ")
-anio = int(input("ingrese el año[>=1900]: "))
+from funcionesCal  import dia_mes
+from funcionesCal  import num_dias_mes
+from funcionesCal  import print_calendario# importamos todas las funciones del modulo funciones_calendario
 
+#mes = input("ingrese el mes [1-12]: ")
+#anio = int(input("ingrese el año[>=1900]: "))
 
-# peticion validada del mes
+# peticion validada del mes, lazo condicionado
 mes_valido = False
 while not mes_valido:
     try:
@@ -11,9 +14,9 @@ while not mes_valido:
             mes_valido = True
             
         else:
-            print("El mes debe ser un número entero entre 1 y 12")
+            print("ERROR: El mes debe ser un número y debe ser entero entre 1 y 12")
     except ValueError:
-        print("El mes debe ser un número entero entre 1 y 12")
+        print("ERROR: El mes debe ser un número entero entre 1 y 12")
 
 #peticion validada del anio
 anio_valido = False
@@ -24,27 +27,33 @@ while not anio_valido:
             anio_valido = True
           
         else:
-            print("El anio debe ser un número entero mayor a 1900")
+            print("ERROR: El anio debe ser un número y debe ser entero mayor a 1900")
     except ValueError:
-        print("El anio debe ser un número entero mayor a 1900")
+        print("ERROR: El anio debe ser un número entero mayor a 1900")
+
+if anio_valido:
+    calendario_anual = []
+    for mes in range(1,13):
+        calendario = [''] * 42
+        idx = dia_mes (1, mes, anio)
+        n_dias = num_dias_mes(mes, anio)
+        calendario [idx:idx+n_dias] = range (1, n_dias+1)
+        calendario_anual.append(calendario)
+
+    print_calendario_anual(anio, calendario_anual)
+
+"""
 
 if mes_valido and anio_valido:
 # mostramos el calendario del mes y del anio
     calendario = [''] * 42
-    idx = dia_mes (1, mes,  anio)
+    idx = dia_mes (1, mes, anio)
     n_dias = num_dias_mes (mes, anio)
     calendario [idx:idx+n_dias] = range (1, n_dias+1)
+    # idx:0 [1,2,3,4 ....,ndias, '','','',''] (42 elementos)
+    # idx:1 ['','','',1,2,3,4 ....,ndias, '','',''] (42 elementos)
 
     print_calendario(mes, calendario)
 
-def dia_mes(dia, mes, anio):
-    #retorna el indice del dia en el calendario (mes/anio). 0:Dom, 1:Lun, 2:Mar, 3:Mie, 4:Jue, 5:Vie, 6:Sab
-    pass
-
-def num_dias_mes(mes, anio):
-    #retorna el numero de dias del mes y anio dados
-    pass
-
-def print_calendario(mes, calendario):
-    #imprime el calendario del mes y anio dados
-    pass
+"""
+       
