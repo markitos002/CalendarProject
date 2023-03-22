20a587e5c68cd3b0c4d91a67a8f6bef87a8a1d5b
Marco Fidel Avila <markos.avila@gmail.com>
2023-03-21 23:55:52 -0500

    fin proyecto calendario

--- CursoCalendario2.py
+++ CursoCalendario2.py
@@ -1,6 +1,66 @@
-from funcionesCal  import dia_mes
-from funcionesCal  import num_dias_mes
-from funcionesCal  import print_calendario# importamos todas las funciones del modulo funciones_calendario
+from funcionesCal  import *
+import sys
+
+
+# >> python3 CursoCalendario2.py 1 2019
+# sys.argv = ['CursoCalendario2.py', '1', '2019']
+
+
+if len(sys.argv) == 1 or len(sys.argv) > 3:
+   error_msg()
+   sys.exit()
+
+if len(sys.argv) == 2:
+    try:
+        anio = int(sys.argv[1])
+        if anio < 1900:
+            error_msg()
+            sys.exit()
+    except ValueError:
+        error_msg()
+        sys.exit()
+
+
+    calendario_anual = []
+    for mes in range(1, 13):
+        calendario = [''] * 42
+        idx = dia_mes (1, mes, anio)
+        n_dias = num_dias_mes(mes, anio)
+        calendario [idx:idx+n_dias] = range (1, n_dias+1)
+        calendario_anual.append(calendario)
+
+    print_calendario_anual(anio, calendario_anual)
+    sys.exit()
+
+if len(sys.argv) == 3:
+    try:
+        mes = int(sys.argv[1])
+        if not mes >= 1 and mes <= 12:
+            error_msg()
+            sys.exit()
+    except ValueError:
+        error_msg()
+        sys.exit()
+
+    try:
+        anio = int(sys.argv[2])
+        if anio < 1900:
+            error_msg.msg()
+            sys.exit()
+    except ValueError:
+        error_msg.msg()
+        sys.exit()
+
+    calendario = [''] * 42
+    idx = dia_mes (1, mes, anio)
+    n_dias = num_dias_mes (mes, anio)
+    calendario [idx:idx+n_dias] = range (1, n_dias+1)
+    print_calendario(mes, calendario)
+    sys.exit()
+
+
+
+"""
 
 #mes = input("ingrese el mes [1-12]: ")
 #anio = int(input("ingrese el año[>=1900]: "))
@@ -18,32 +78,6 @@ while not mes_valido:
     except ValueError:
         print("ERROR: El mes debe ser un número entero entre 1 y 12")
 
-#peticion validada del anio
-anio_valido = False
-while not anio_valido:
-    try:
-        anio = int(input ("ingrese el anio :"))
-        if anio > 1900:
-            anio_valido = True
-          
-        else:
-            print("ERROR: El anio debe ser un número y debe ser entero mayor a 1900")
-    except ValueError:
-        print("ERROR: El anio debe ser un número entero mayor a 1900")
-
-if anio_valido:
-    calendario_anual = []
-    for mes in range(1,13):
-        calendario = [''] * 42
-        idx = dia_mes (1, mes, anio)
-        n_dias = num_dias_mes(mes, anio)
-        calendario [idx:idx+n_dias] = range (1, n_dias+1)
-        calendario_anual.append(calendario)
-
-    print_calendario_anual(anio, calendario_anual)
-
-"""
-
 if mes_valido and anio_valido:
 # mostramos el calendario del mes y del anio
     calendario = [''] * 42
@@ -54,6 +88,20 @@ if mes_valido and anio_valido:
     # idx:1 ['','','',1,2,3,4 ....,ndias, '','',''] (42 elementos)
 
     print_calendario(mes, calendario)
+
+
+#peticion validada del anio
+anio_valido = False
+while not anio_valido:
+    try:
+        anio = int(input ("ingrese el anio :"))
+        if anio > 1900:
+            anio_valido = True
+          
+        else:
+            error_msg()
+    except ValueError:
+            error_msg()
 
 """
        
--- funcionesCal.py
+++ funcionesCal.py
@@ -1,3 +1,16 @@
+def error_msg():
+    print()
+    print("Uso: CursoCalendario2.py <mes> <anio>")
+    print("Uso: CursoCalendario2.py <anio>")
+    print()
+    print("   <mes> Valor entero entre 1 y 12")
+    print("   <anio> Valor entero mayor o igual a 1900")
+    print()
+    print("Ejemplo: CursoCalendario2.py 3 2023")
+    print("Ejemplo: CursoCalendario2.py 2023")
+    print()
+    return None
+
 
 
 def dia_mes(dia, mes, anio):
@@ -25,23 +38,52 @@ def num_dias_mes(mes, anio):
     else:
         return 31
 
+def print_calendario_anual(anio, calendario_anual):
+    #imprime una lista de calendarios (12 elementos) en formato de calendario anual
+    nombre_mes = {0:"ENERO", 1:"FEBRERO", 2:"MARZO", 3:"ABRIL", 
+                  4:"MAYO", 5:"JUNIO", 6:"JULIO", 7:"AGOSTO", 
+                  8:"SEPTIEMBRE", 9:"OCTUBRE", 10:"NOVIEMBRE", 
+                  11:"DICIEMBRE"}
+    print(f"{anio:^80}")
+    print()
+    for mes in range(0,12,3):
+        print(f"{nombre_mes[mes]:30}", end='')
+        print(f"{nombre_mes[mes+1]:30}", end='')
+        print(f"{nombre_mes[mes+2]:30}")
+
+        print(f"{' D  L  M  M  J  V  S':30}", end='')
+        print(f"{' D  L  M  M  J  V  S':30}", end='')
+        print(f"{' D  L  M  M  J  V  S':30}")
+
+        for i in range(0, 42, 7):
+            print("{:2} {:2} {:2} {:2} {:2} {:2} {:2}          ".format(*calendario_anual[mes][i:i+7]), end='')  #funcion splat (*)
+            print("{:2} {:2} {:2} {:2} {:2} {:2} {:2}          ".format(*calendario_anual[mes+1][i:i+7]), end='')
+            print("{:2} {:2} {:2} {:2} {:2} {:2} {:2}          ".format(*calendario_anual[mes+2][i:i+7]))
+        
+        print()
+
 
 def print_calendario(mes, calendario):
     #imprime una lista de dias (42 elementos) en formato de calendario mensual
-    nombre_mes = {1:'Enero', 2:'Febrero', 3:'Marzo', 4:'Abril', 5:'Mayo', 6:'Junio', 7:'Julio', 8:'Agosto', 9:'Septiembre', 10:'Octubre', 11:'Noviembre', 12:'Diciembre'}
+    nombre_mes = {1:'Enero', 2:'Febrero', 3:'Marzo', 4:'Abril', 
+                  5:'Mayo', 6:'Junio', 7:'Julio', 8:'Agosto', 
+                  9:'Septiembre', 10:'Octubre', 11:'Noviembre', 12:'Diciembre'}
     print(nombre_mes[mes])
     print(" D  L  M  M  J  V  S")
+    
     for idx, dia in enumerate(calendario, start=1):
         print(f"{dia:2}", end=" ")
         if idx % 7 == 0:
             print()
 
-def print_calendario_anual(anio, calendario_anual):
-    #imprime una lista de calendarios (12 elementos) en formato de calendario anual
-    nombre_mes = {1:'Enero', 2:'Febrero', 3:'Marzo', 4:'Abril', 5:'Mayo', 6:'Junio', 7:'Julio', 8:'Agosto', 9:'Septiembre', 10:'Octubre', 11:'Noviembre', 12:'Diciembre'}
+
+
+
+"""
     print(nombre_mes[mes])
     print(" D  L  M  M  J  V  S")
     for idx, dia in enumerate(calendario, start=1):
         print(f"{dia:2}", end=" ")
         if idx % 7 == 0:
             print()
+"""
